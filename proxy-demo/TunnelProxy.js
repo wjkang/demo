652af265b6936e1145df468fc12d166a2dff55e2
Administrator <977865769@qq.com>
2016-10-11 16:16:47 +0800

    proxy

---
+++ proxy-demo/TunnelProxy.js
@@ -0,0 +1,21 @@
+/**
+ * Created by wjkang on 2016/10/11.
+ */
+var http = require('http');
+var net = require('net');
+var url = require('url');
+
+function connect(cReq, cSock) {
+    var u = url.parse('http://' + cReq.url);
+
+    var pSock = net.connect(u.port, u.hostname, function() {
+        cSock.write('HTTP/1.1 200 Connection Established\r\n\r\n');
+        pSock.pipe(cSock);
+    }).on('error', function(e) {
+        cSock.end();
+    });
+
+    cSock.pipe(pSock);
+}
+
+http.createServer().on('connect', connect).listen(8889, '0.0.0.0');
