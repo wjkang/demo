313d97f950d6b8dbbd978553f6a2eeacec3f3873
username <977865769@qq.com>
2016-12-11 22:33:24 +0800

    默认的

---
+++ b.js
@@ -0,0 +1,2 @@
+
+shjdhsdj
--- server.js
+++ server.js
@@ -3,27 +3,31 @@ const fs = require('fs');
 const hostname = '127.0.0.1';
 const port = 8080;
 http.createServer((req, res) => {
-    if(req.url=='/hh.js'){
-        fs.readFile('hh.js','utf-8',function(err,data){
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            setTimeout( function(){res.write(data);
-                res.end()},10000)
+    if (req.url == '/a.js') {
+        fs.readFile('a.js', 'utf-8', function (err, data) {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            setTimeout(function () {
+                res.write(data);
+                res.end()
+            }, 10000)
         })
-    }else if(req.url=='/bb.js'){
-        fs.readFile('bb.js','utf-8',function(err,data){
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
+    } else if (req.url == '/b.js') {
+        fs.readFile('b.js', 'utf-8', function (err, data) {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
             res.write(data);
             res.end()
         })
-    }else if( req.url == '/red.css' ){
-        fs.readFile('red.css','utf-8',function(err,data){
-            res.writeHead(200, { 'Content-Type': 'text/css' });
-            res.write(data);
-            res.end()
+    } else if (req.url == '/style.css') {
+        fs.readFile('style.css', 'utf-8', function (err, data) {
+            res.writeHead(200, {'Content-Type': 'text/css'});
+            setTimeout(function () {
+                res.write(data);
+                res.end()
+            }, 10000)
         })
-    }else if(req.url == '/index.html'){
-        fs.readFile('index.html','utf-8',function(err,data){
-            res.writeHead(200, { 'Content-Type': 'text/html' });
+    } else if (req.url == '/index.html') {
+        fs.readFile('index.html', 'utf-8', function (err, data) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(data);
             res.end()
         })
